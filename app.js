6ee9a0d5223afb8b48e02ef26678e933e0b75173
ginnygu <ginnygu@gmail.com>
2023-05-16 14:30:11 -0400

    added index.html, added fs and about page

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
-// http built-in module
+// http, fs  built-in node module
 
 const http = require("http");
+const fs = require("fs");
 const port = 3000; //between 3000 - 5000 are available
 
 const server = http.createServer((request, response) => {
+	// req, res
 	const url = request.url;
 	console.log(request.url);
 	if (url === "/about") {
-		return response.end("about page");
+		fs.readFile("./public/indexs.html", (error, data) => {
+			if (error) {
+				console.log(error.message);
+				response.end(error.message);
+			} else {
+				response.writeHeader(200, { "content-type": "text/html" });
+				response.write(data);
+				response.end();
+			}
+		});
+	} else {
+		response.writeHeader(400);
+		response.end("No page found");
 	}
-	response.end("Hello Ginny");
 });
 
 server.listen(port, () => {
 	console.log(`listening to port ${port}`);
 });
+
+// - create another html page called contact
+// - create another if statement that if the route matches /contact then repond with the page that you just made.
+// - if there's any error, display the error on the page not console.log
+
+// - create another html page called whatever you like
+// - create another if statement that if the route matches /_______ then repond with the page that you just made.
+// - if there's any error, display the error on the page not console.log
